86415ebd2153e761e744af96ef70b81d06a725fa
jvlsg <jvlsg@users.noreply.github.com>
2019-11-08 15:47:24 -0300

    UPD: Updated the constraints on the model

--- tsp.jl
+++ tsp.jl
@@ -5,38 +5,30 @@ using JuMP, Cbc
 """
 nxn Matrix of ints or floats representing the distances between the nodes
 """
-node_distances = [ [0,1] , [1,0] ]
+node_distances = []
 
 node_number = size(node_distances)[1]
 
-
-b = [-5;  10]
-c = [ 1  2  5]
-
-
 tsp_model = Model(with_optimizer(Cbc.Optimizer))
 
-# para definir o tempo limite em segundos.
-# set_time_limit_sec(tsp_model , <tempo>)
+#Limits execution time
+set_time_limit_sec(tsp_model , 1)
 
-"""
-Decision variable - binary nxn Matrix representing the edges taken between nodes
-"""
+#Decision variable - binary nxn Matrix representing the edges taken between nodes
 @variable(tsp_model, edges_taken[1:node_number, 1:node_number], Bin)
 @variable(tsp_model, u[1:node_number], integer=true)
 
-fix(u[1], 0) # change the 1 for the position of initial node
-
-"""
-Min Sum ( node_distances[i][j] * edges_taken[i][j] for all nodes)
-"""
+#Min Sum ( node_distances[i][j] * edges_taken[i][j] for all nodes)
 @objective(tsp_model,Min, sum( map(*,node_distances,edges_taken) ) )
 
+fix(u[1], 0) # change the 1 for the position of initial node
+
 @constraints tsp_model begin
-# TODO - Add constraints
-    [i = 1:node_numbers] , sum(x[i,1:end .!=i]) == 1
-    [i = 1:node_numbers] , sum(x[1:end .!=i,i]) == 1
-    [i = 1:node_numbers , j= 2:node_numbers , i != j] , u[i] - u[j] + node_number*edges_taken[i,j] <= node_number-1
+    [i= 1:node_number] , sum(edges_taken[i,1:end .!=i]) == 1
+    [i= 1:node_number] , sum(edges_taken[1:end.!=i,i]) == 1
+    [i = 1:node_number , j= 2:node_number , i != j] , u[j] >= u[i] + edges_taken[i,j] - node_number *(1-edges_taken[i,j])
 end
 
-print(tsp_model)
+#print(tsp_model)
+optimize!(tsp_model)
+print(edges_taken)
