ec7dc419d310b604b368a886411de9f2ae4b381d
jvlsg <jvlsg@users.noreply.github.com>
2019-11-08 18:00:19 -0300

    FIX: distance from coords functions ADD: added main function ADD: Args for the file UPD: Parser removes unwelcome text

--- datasets/parser.sh
+++ datasets/parser.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wc -l $1 | cut -f 1 -d " " | xargs grep "1 " $1 -A | cut -f 2,3 -d " "
+wc -l $1 | cut -f 1 -d " " | xargs grep "1 " $1 -A | sed -r 's/NODE_COORD_SECTION//;s/EOF//' | cut -f 2,3 -d " "
--- tsp.jl
+++ tsp.jl
@@ -1,13 +1,18 @@
 using JuMP, Cbc, DelimitedFiles
 
-function distances_from_coords(node_coords)
-    
+function distances_from_coords(node_coords::Array{Float32,2})
+    """
+    Recieves a 2D array of the X,Y coordinates for each of the n nodes. 
+    E.g.
+    node_coords[node,1] : Latitude of the node
+
+    Returns a n,n matrix of the euclidean distances between the nodes
+    """
     node_number = size(node_coords,1)
-    node_distances = zeros()
-    
+    node_distances = zeros(node_number,node_number)
     for row in range(1,length=node_number)
         for column in range(1,length=node_number)
-            #Applying Euclidean distance, http://www.math.uwaterloo.ca/tsp/world/countries.html#DJ
+            #Applying Euclidean distance
             node_distances[row,column] = sqrt(  
                 Float32(node_coords[column,1]-node_coords[row,1])^2 + 
                 Float32(node_coords[column,2]-node_coords[row,2])^2 )  
@@ -16,42 +21,48 @@ function distances_from_coords(node_coords)
 
     return node_distances
 end
-"""
-nxn Matrix of ints or floats representing the distances between the nodes
-"""
-node_coords = DelimitedFiles.readdlm("datasets/parsed_dj38.tsp",' ',Float32,'\n')
 
+function tsp_main(args)
+    """
+    Use Cbc to calculate Travelling Salesperson solution for a given graph.
+    Args: Filename of the parsed graph file. 
+        The file should follow this standard 
+        http://www.math.uwaterloo.ca/tsp/world/countries.html#DJ
 
-node_distances = [ 0  186 105 208 177 94 ;
-187 0 89.8 223 255 254 ;
-99.9 89 0  203 204 172 ;
-206 220 203 0 377 295 ; 
-168 251 201 376 0 156 ;
-86.8 255 173  293 159  0]
-readdlm()
+    """
 
-node_number = size(node_distances)[1]
+    node_coords = DelimitedFiles.readdlm(args[1],' ',Float32,'\n')
+    
+    """
+    nxn Matrix of floats representing the distances between the nodes
+    """
+    node_distances = distances_from_coords(node_coords)
 
-tsp_model = Model(with_optimizer(Cbc.Optimizer))
+    node_number = size(node_distances)[1]
 
-#Limits execution time
-set_time_limit_sec(tsp_model , 1)
+    tsp_model = Model(with_optimizer(Cbc.Optimizer))
 
-#Decision variable - binary nxn Matrix representing the edges taken between nodes
-@variable(tsp_model, edges_taken[1:node_number, 1:node_number], Bin)
-@variable(tsp_model, u[1:node_number], integer=true)
+    #Limits execution time
+    set_time_limit_sec(tsp_model , 1)
 
-#Min Sum ( node_distances[i][j] * edges_taken[i][j] for all nodes)
-@objective(tsp_model,Min, sum( map(*,node_distances,edges_taken) ) )
+    #Decision variable - binary nxn Matrix representing the edges taken between nodes
+    @variable(tsp_model, edges_taken[1:node_number, 1:node_number], Bin)
+    @variable(tsp_model, u[1:node_number], integer=true)
 
-fix(u[1], 0) # change the 1 for the position of initial node
+    #Min Sum ( node_distances[i][j] * edges_taken[i][j] for all nodes)
+    @objective(tsp_model,Min, sum( map(*,node_distances,edges_taken) ) )
 
-@constraints tsp_model begin
-    [i= 1:node_number] , sum(edges_taken[i,1:end .!=i]) == 1
-    [i= 1:node_number] , sum(edges_taken[1:end.!=i,i]) == 1
-    [i = 1:node_number , j= 2:node_number , i != j] , u[j] >= u[i] + edges_taken[i,j] - node_number *(1-edges_taken[i,j])
+    fix(u[1], 0) # change the 1 for the position of initial node
+
+    @constraints tsp_model begin
+        [i= 1:node_number] , sum(edges_taken[i,1:end .!=i]) == 1
+        [i= 1:node_number] , sum(edges_taken[1:end.!=i,i]) == 1
+        [i = 1:node_number , j= 2:node_number , i != j] , u[j] >= u[i] + edges_taken[i,j] - node_number *(1-edges_taken[i,j])
+    end
+
+    #print(tsp_model)
+    optimize!(tsp_model)
+    print(edges_taken)
 end
 
-#print(tsp_model)
-optimize!(tsp_model)
-print(edges_taken)
+tsp_main(ARGS)
